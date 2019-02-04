404996d8150844b486414195681bef13d3308df9
miguel <gutierrez.miguel@correounivalle.edu.co>
2019-02-04 11:38:41 -0500

    inventario
    se agregaron los atributos y gets

--- src/Modelo/Inventario.java
+++ src/Modelo/Inventario.java
@@ -10,5 +10,32 @@ package Modelo;
  * @author juandavid
  */
 public class Inventario {
+    private String nProducto;
+    private String idProducto;
+    private String valor;    
+    private String cantidadDisponible;
+    
+    public Inventario(String nProducto,String idProducto,String valor,String cantidadDisponible){
+        this.nProducto=nProducto;
+        this.idProducto=idProducto;
+        this.valor=valor;
+        this.cantidadDisponible=cantidadDisponible;
+    }
+    
+    public String GetNProducto(){
+        return nProducto;
+    }
+    
+    public String GetIdProducto(){
+        return idProducto;
+    }
+    
+    public String GetValor(){
+        return valor;
+    }
+    
+    public String GetCantidadDisponible(){
+        return cantidadDisponible;
+    }
     
 }
