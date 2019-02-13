910f473ae63468b522fcf202fa1c248198f7b60e
miguel <gutierrez.miguel@correounivalle.edu.co>
2019-02-13 09:08:50 -0500

    carritos, vista agregar sede

---
+++ src/Modelo/CarritoCot.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CarritoCot {
+    private String id_cotizacion;
+    private String id_producto;
+    private int cantidad;
+    private float subtotal;
+
+    public CarritoCot(String id_cotizacion, String id_producto, int cantidad, float subtotal) {
+        this.id_cotizacion = id_cotizacion;
+        this.id_producto = id_producto;
+        this.cantidad = cantidad;
+        this.subtotal = subtotal;
+    }
+
+    public String getId_cotizacion() {
+        return id_cotizacion;
+    }
+
+    public void setId_cotizacion(String id_cotizacion) {
+        this.id_cotizacion = id_cotizacion;
+    }
+
+    public String getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(String id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public float getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(float subtotal) {
+        this.subtotal = subtotal;
+    }
+    
+}
