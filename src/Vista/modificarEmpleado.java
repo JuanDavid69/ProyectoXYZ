d41375409b0b4a0b47e4d25b7aed2020b18ff7da
Juan David <juan.gomez.orozco@correounivalle.edu.co>
2019-03-03 22:23:19 -0500

    algunas validasiones

--- src/Vista/modificarEmpleado.java
+++ src/Vista/modificarEmpleado.java
@@ -114,7 +114,22 @@ public class modificarEmpleado extends javax.swing.JFrame {
 
         jLabel9.setText("N° Sede");
 
+        sede.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                sedeKeyTyped(evt);
+            }
+        });
+
         jLabel10.setText("Telefono");
+
+        telefono.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                telefonoKeyReleased(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                telefonoKeyTyped(evt);
+            }
+        });
 
         jLabel11.setText("Cedula");
 
@@ -133,6 +148,12 @@ public class modificarEmpleado extends javax.swing.JFrame {
         jLabel6.setText("Cargo");
 
         jLabel7.setText("Salario");
+
+        salario.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                salarioKeyTyped(evt);
+            }
+        });
 
         jLabel14.setText("Fecha De Inicio");
 
@@ -214,7 +235,7 @@ public class modificarEmpleado extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 241, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(seleccionar)
                             .addComponent(cerrar)))
                     .addGroup(layout.createSequentialGroup()
@@ -257,11 +278,11 @@ public class modificarEmpleado extends javax.swing.JFrame {
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                 .addComponent(estado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addComponent(jLabel12)))
-                        .addGap(18, 18, 18)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(contraseña, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel13))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 23, Short.MAX_VALUE)
                 .addComponent(Modificar)
                 .addContainerGap())
         );
@@ -331,6 +352,37 @@ public class modificarEmpleado extends javax.swing.JFrame {
         String busqueda = this.busqueda.getText();
         cargarUsuarios(busqueda);
     }//GEN-LAST:event_busquedaKeyReleased
+
+    private void salarioKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_salarioKeyTyped
+        char u = evt.getKeyChar();
+        if(Character.isLetter(u)){
+            getToolkit().beep();
+            evt.consume();
+            JOptionPane.showMessageDialog(this, "Solo caracteres numericos");
+        }
+    }//GEN-LAST:event_salarioKeyTyped
+
+    private void sedeKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_sedeKeyTyped
+        char u = evt.getKeyChar();
+        if(Character.isLetter(u)){
+            getToolkit().beep();
+            evt.consume();
+            JOptionPane.showMessageDialog(this, "Solo caracteres numericos");
+        }
+    }//GEN-LAST:event_sedeKeyTyped
+
+    private void telefonoKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_telefonoKeyReleased
+
+    }//GEN-LAST:event_telefonoKeyReleased
+
+    private void telefonoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_telefonoKeyTyped
+        char u = evt.getKeyChar();
+        if(Character.isLetter(u)){
+            getToolkit().beep();
+            evt.consume();
+            JOptionPane.showMessageDialog(this, "Solo caracteres numericos");
+        }
+    }//GEN-LAST:event_telefonoKeyTyped
 
     /**
      * @param args the command line arguments
