7a8793380d0df63eb8aa2ef8c236e91ca2c58551
Juan David <juan.gomez.orozco@correounivalle.edu.co>
2019-03-03 21:38:35 -0500

    cosas nuevas en la base

--- implementacionBD.sql
+++ implementacionBD.sql
@@ -5,7 +5,8 @@ CREATE TABLE sedes
 	nombre VARCHAR(30) NOT NULL,
 	direccion VARCHAR(30) NOT NULL,
 	ciudad VARCHAR(30) NOT NULL,
-	telefono VARCHAR(30) NOT NULL
+	telefono VARCHAR(30) NOT NULL,
+	estado VARCHAR(30) NOT NULL
 
 );
 
@@ -99,11 +100,17 @@ CREATE TABLE ordenes
 
 
 
-INSERT INTO sedes VALUES ('01', 'Sede sur', 'Carrear 77A #2B-34','Cali', '123456');
+INSERT INTO sedes VALUES ('01', 'Sede sur', 'Carrear 77A #2B-34','Cali', '123456', 'Activa');
+INSERT INTO sedes VALUES ('02', 'Sede norte', 'Calle 1','Bogota', '654321', 'Activa');
+INSERT INTO sedes VALUES ('03', 'Sede oriente', 'Calle 2','Cali', '111111', 'Activa');
+INSERT INTO sedes VALUES ('04', 'Sede occidente', 'Calle 3','Bogota', '2222222', 'Activa');
 
 INSERT INTO usuarios VALUES ('111','111' ,'Activo','Santiago','Gerente' ,'$100000','31-07-2005' ,'Calle 5 #3C', '01' , '654321');
 INSERT INTO usuarios VALUES ('222','222' ,'Activo','Miguel','Vendedor' , '$50000','20-01-2005', 'Carrear 80 #2B-102', '01' , '123123');
 INSERT INTO usuarios VALUES ('333','333' ,'Activo','Juan','Jefe de taller' , '$25000','01-07-2011', 'Carrear 45 3D', '01' , '567890');
+INSERT INTO usuarios VALUES ('444','444' ,'Activo','Juan Pablo','Gerente' ,'$100000','03-03-2019' ,'Calle 5 #3C', '02' , '654321');
+INSERT INTO usuarios VALUES ('555','555' ,'Activo','Walter','Vendedor' , '$50000','03-03-2019', 'Carrear 80 #2B-102', '02' , '123123');
+INSERT INTO usuarios VALUES ('666','666' ,'Activo','Andres','Jefe de taller' , '$25000','03-03-2019', 'Carrear 45 3D', '02' , '567890');
 
 INSERT INTO inventario VALUES ('001', 'Silla', 20, 100);
 INSERT INTO inventario VALUES ('002', 'Sillon', 10, 650);
@@ -112,9 +119,15 @@ INSERT INTO inventario VALUES ('004', 'Nochero', 30, 470);
 INSERT INTO inventario VALUES ('005', 'Comedor', 15, 2200);
 INSERT INTO inventario VALUES ('006', 'Tocador', 9, 700);
 INSERT INTO inventario VALUES ('007', 'Escritorio', 11, 1500);
+INSERT INTO inventario VALUES ('008', 'Cama', 16, 750);
+INSERT INTO inventario VALUES ('009', 'Cuna', 2, 1050);
 
 INSERT INTO ventas VALUES ('001', '222', '04-02-2019', 400);
 
 INSERT INTO cotizaciones VALUES ('001', '222', '04-02-2019', 600);
 
 INSERT INTO ordenes VALUES ('001', '333', '001', 5, '04-02-2019', 'Sin aprobar');
+INSERT INTO ordenes VALUES ('002', '333', '002', 10, '04-02-2019', 'Sin aprobar');
+INSERT INTO ordenes VALUES ('003', '333', '003', 15, '04-02-2019', 'Sin aprobar');
+INSERT INTO ordenes VALUES ('004', '333', '004', 56, '04-02-2019', 'Sin aprobar');
+INSERT INTO ordenes VALUES ('005', '333', '005', 100, '04-02-2019', 'Sin aprobar');
